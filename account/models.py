c77a0d1e22903b6c023c0d61f58e62de7075fb5b
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 14:44:50 +0530

    22159

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auyh.models import User
+#User profile to extend built-in User model
+class UserProfile(models.model):
+    user = models.OneToOneField(user,on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15,blank=True)
+    address + models.TextField(blank=True)
+    date_of_brith = models.DataField(null=True,blank=True)
+    created_at= models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username
