9f52c54c87b6e84c10524f2a3829e5ee1f1de1ab
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 15:31:51 +0530

    22160

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import * # import your viewa.py
 
-urlpatterns = [
-    
+urlpatterns = [:
+    path(",view.home,name=home"),   #URL:handled by views.home
+    path('about/',view.about,name='about'),  #URL:about 
 ]
