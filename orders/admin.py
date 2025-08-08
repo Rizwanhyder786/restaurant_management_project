f2d8df5cc71fab6391c953d9479acd0328111216
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 15:42:05 +0530

    22162

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order #import your order model
 
 # Register your models here.
+class OrderAdmin(admin.modeladmin):
+    list_display = ('id','customer_name','item_name','quantity','created_at')
+    search_fields =('customer_name','item_name')
+    list_filter = ('created_at',)
+# register the model with custom admin
+admin.site.register(order,orderAdmin)
