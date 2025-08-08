c73f6683bb7e6a6625438a097a3ed58ffcd08428
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 15:06:16 +0530

    22159

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+#login view
+def login_view(request):
+    return HttpResponse("this is the login page.")
+#resister view
+def resister_view(request):
+    return HttpResponse("This is the Registration page.")
+#profile view
+def profile_view(request):
+    return HttpResponse("this is the user profile page")
