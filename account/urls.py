92cf26aefeef607c06a69fd670ba2861e065c72f
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 14:59:28 +0530

    22159

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('login/',views.login_view,name='login'),
+    path('register/',views.register_view,name='register'),
+    path('profile/', views.profile_view, name='profile'),
 ]
