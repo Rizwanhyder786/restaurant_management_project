48c495ff2174eb7954a8f2ce208dcdfd01a7d82b
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 15:24:28 +0530

    22160

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class item(models.model):
+    name=models.CharField(max_length=100)
+    price=models.DecimalField(max_digits=6,decimal_places=2)
+    description= models.TextField(blank=True)
+    is_available=models.BooleanField(default=True)
+    created_at = models.DateTimeFeild(auto_now_add=True)
+    def __str__(self):
+        return self.name
