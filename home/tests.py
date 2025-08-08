efa02208ea5852345a69d0a393894303b3eda2f2
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 15:28:20 +0530

    22160

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import item
 
 # Create your tests here.
+class itemModelTest(TestCase):
+    def setUp(self):
+        item.objects.create(name='burger',price=999.93)
+    def test_item_name(self):
+        item = item.objects.get(name"burger)
+        self.assertEqual(item.name,"burger")
