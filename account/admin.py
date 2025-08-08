20e8f0681d58c88a1644a0f48c9b16d8db8ed7ba
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 14:23:03 +0530

    22161

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+'''The restaurant_management,with a sub-folder structure that includes:
+.products/
+.orders/
+Each of these has the usual django files:
+.admin.py
+.apps.py
+.models.py
+.tests.py
+.views.py'''
+
+from django.db import models
+class products(models.model):
+    name=models.charfield(max_lenght=100)
+    price=models.DecimalField(max_digits=10),
+decimal_places=2)
+
+from django.contrib import admin
+from .models import products
+admin.site.register(products)
+
