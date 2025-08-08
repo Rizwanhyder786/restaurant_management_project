d6a9a252daba825faaf2e9d34e8fb9bf04c0d42f
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-08 14:56:29 +0530

    22159

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+class UserProfileTestCase(TestCase):
+    def setUp(self):
+        #create a sample user and user profile
+        self.user = user.objects.create_user(username='testuser',password='testpass')
+        self.profile = UserProfile.objects.create(
+            user=self.user,
+            phone='1234567890',
+            address='Hyderabad,India',
+            date_of_birth='2002-01-01'
+        )
+        def test_profile_created(self):
+            #tesr if the profile is linked to the correct user
+            self.assertEqual(self.profile.user.username.'testuser')
+            self.assertEqual(self.profile.phone,'1234567890')
