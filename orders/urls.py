dedceb1f79666de9560684b43db35064a89a9099
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-20 16:33:24 +0530

    30160

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('admin/',admin.site.urls),
+    path('order/',include('orders.urls')),#includes order app urls
 ]
