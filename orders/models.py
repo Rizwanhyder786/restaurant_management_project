fb78e1fbca51f9056461aef6404eede1a6ec0785
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-20 16:21:02 +0530

    30160

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.model):
+    customer_name =models.Charfield(max_length=100)
+    product_name=models.Charfield(max_length=100)
+    quantity=models.PostiveIntegerField()
+    price=models.DeciamlField(max_digits=10),decimal_places=2)
+    created_at =models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.custumer_name}-{self.product_name}"
