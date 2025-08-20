57f0c2ec37e7c32653b68596c876eec85c10e05d
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-08-20 16:31:00 +0530

    30160

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Order
 
 # Create your tests here.
+
+class OrderModelTest(TestCase):
+    def setup(self):
+        #create a sample order before each test
+        Order.objects.create(
+            customer_name="Rizwan",
+            product_name="Laptop",
+            qunanity=2,
+            price=50000.00
+        )
+    def test_order_created(self):
+        '''check if the order is saved correctly'''
+        order=order.objects.get(customer_name="Rizwan")
+        self.assertEqual(order.product_name, "Laptop")
+        self.assertEqual(orderquantity,2)
+        self.assertEqual(float(order.price),50000.00)
+    def test_str_method(self):
+        '''check if __str__ return correct format'''
+        order =order.objects.get(customer_name="Rizwan")
+        self.assertEqual(str(order),"Rizwan - Laptop")
