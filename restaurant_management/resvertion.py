699597124baf4cc3a6db35a6249ec5b01815f317
Rizwan Hyder <99253184+Rizwanhyder786@users.noreply.github.com>
2025-09-05 22:37:19 +0530

    39831

--- restaurant_management/resvertion.py
+++ restaurant_management/resvertion.py
@@ -11,3 +11,45 @@ def current_year(request):
         'year':datetime.now().year
     }
 
+body{
+    margin:0;
+    padding:0;
+    front-family:Arial,helvetica,sams-serif;
+    background-color:#f9f9f9f
+    color:#333
+}
+header{
+    background-color:white;
+    color:black;
+    text-algin:center;
+    padding:1,5rem;
+}
+h1{
+    margin:0;
+    font-size:2,rem;
+}
+main{
+    padding:2rem;
+    max-width:900;
+    margin:0 auto;
+}
+p{
+    font-size:1.1rem;
+    line-heigth:1.6;
+    margin-bottom:1rem;
+}
+a{
+    color:black;
+    text-decoration:underline;
+}
+a:hover{
+    text-decoration:underline;
+}
+footer{
+    margin-top:2rem;
+    background:blue;
+    color:white;
+    text-algin:cenetr;
+    padding:1rem;
+    font-size:0,8rem;
+}
